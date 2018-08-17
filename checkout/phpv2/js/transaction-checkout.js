986d1ec6185bf146a2a9e75c6cc5ed358fd1f6f8
Murilo Henrique <murilohns@gmail.com>
2018-08-17 16:00:47 -0300

    checkout: add helpers to show error and success messages

--- checkout/phpv2/js/transaction-checkout.js
+++ checkout/phpv2/js/transaction-checkout.js
@@ -66,24 +66,4 @@
       ]
     })
   })
-
-  /* Essa função é responsável por criar a Div e exibir o erro ao usuário */
-  function createResponseDiv(elementClass, message) {
-    var responseDiv = document.createElement('div')
-    responseDiv.className = 'response ' + elementClass
-    var responseMessage = document.createTextNode(message)
-    responseDiv.appendChild(responseMessage)
-
-    var body = document.querySelector('body')
-    body.appendChild(responseDiv)
-
-    removeElementAfterFiveSeconds(responseDiv)
-  }
-
-  function removeElementAfterFiveSeconds(element) {
-    setTimeout(function() {
-      element.parentNode.removeChild(element)
-    }, 5000)
-  }
-
 })(document, window)
